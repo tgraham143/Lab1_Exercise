76afe6ce5263997cac8a0a6066199bdbd12783ec
Tanner Graham <james.graham@jeffersonstate.edu>
2023-02-17 13:50:08 -0600

    Changes to prevent infinite loop

--- Infinite_Loop/Code/Code.cpp
+++ Infinite_Loop/Code/Code.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
@@ -9,45 +11,63 @@ int main()
 	int choice = 0;
 
 	string name = "";
-	string age = "";
+	//string age = "";
+	int age = -1;
 	string occup = "";
 
 	while (choice != -1)
 	{
-		cout << "-1: Exit\n";
+		cout << "\n-1: Exit\n";
 		cout << "1: Enter Name\n";
 		cout << "2: Enter Age\n";
 		cout << "3: Enter Occupation\n";
 		cin >> choice;
 
-		/*if (choice == -1)
-			break;*/
 
 		switch (choice)
 		{
+		case -1:
+			break;
 		case 1:
 			cout << "What is your Name: ";
-			cin >> name;
+			//cin >> name;
+			cin.ignore();
+			getline(cin, name);
 			break;
 		case 2:
 			cout << "What is your Age? ";
 			cin >> age;
+
+			if (cin.fail()) {
+				cout << "Invalid Age value.\n";
+				cin.clear();
+				age = -1;
+			}
+
+			cin.ignore(1000, '\n');
 			break;
 		case 3:
 			cout << "What is your Occupation? ";
-			cin >> occup;
+			//cin >> occup;
+			cin.ignore();
+			getline(cin, occup);
 			break;
 		default:
 			// Assume Invalid Menu Choice
-			cout << "Sorry that choice is not valid!";
+			cout << "Sorry that choice is not valid!\n";
+
+			if (cin.fail()) {
+				cin.clear();
+				cin.ignore(1000, '\n');
+			}
 			break;
 		}
 	}
 
 	age += 1;
 
-	cout << "Thank you for using our application, " << name << " and hope your career in " << occup << " is successful.";
-	cout << "Hope your " << age << " birthday will enjoyable and exciting";
+	cout << "\nThank you for using our application, " << name << ", and hope your career in " << occup << " is successful.";
+	cout << " Hope your " << age << " birthday will be enjoyable and exciting.";
 
 	return 0;
 }
